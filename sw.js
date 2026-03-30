e463e40dcfc82ca7807e60294d39a00903d00aca
c3dprints <chris@c3dprints.com>
2026-03-30 19:52:44 -0400

    1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'c3d-calc-v3';
+const CACHE = 'c3d-calc-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
